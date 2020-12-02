be284ecd6011130ae99ded075210e368c4a5c78c
Gerson E. Aguirre <hola@gersonaguirre.me>
2020-12-01 22:42:39 -0600

    SERVER COMPLETE

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  return res.send("Ola k ase?");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
